bdfe1fbc6b93d9257f0b119e7a53e78af743904b
Julian Janicki <janickijuliann@gmail.com>
2024-03-27 17:08:08 +0100

    Adding a new view for testing the BLE connection.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
-import { NativeScriptModule } from '@nativescript/angular'
-
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "@nativescript/angular";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BluetoothComponent } from "./bluetooth/bluetooth.component";
 
 @NgModule({
   bootstrap: [AppComponent],
   imports: [NativeScriptModule, AppRoutingModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, BluetoothComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppModule {}
---
+++ src/app/bluetooth/bluetooth.component.ts
@@ -0,0 +1,49 @@
+import { Component, NgZone } from "@angular/core";
+import { Bluetooth, Peripheral } from "@nativescript-community/ble";
+
+@Component({
+  selector: "Bluetooth",
+  templateUrl: "./bluetooth.component.html",
+  styleUrls: ["./bluetooth.component.css"],
+})
+export class BluetoothComponent {
+  private bluetooth = new Bluetooth();
+  public devices: Array<Peripheral> = [];
+  private deviceSet = new Set<string>();
+  public isScanning = false;
+  private scanInterval: any;
+
+  constructor(private zone: NgZone) {}
+
+  public toggleScan() {
+    if (this.isScanning) {
+      this.bluetooth.stopScanning();
+      clearInterval(this.scanInterval);
+    } else {
+      this.devices = [];
+      this.deviceSet.clear();
+      let tempDevices = [];
+      this.bluetooth.startScanning({
+        onDiscovered: (peripheral: Peripheral) => {
+          if (peripheral.name === "HMSoft") {
+            if (this.deviceSet.has(peripheral.UUID)) {
+              let existingDevice = tempDevices.find(
+                (device) => device.UUID === peripheral.UUID
+              );
+              existingDevice.RSSI = peripheral.RSSI;
+            } else {
+              tempDevices.push(peripheral);
+              this.deviceSet.add(peripheral.UUID);
+            }
+          }
+        },
+      });
+      this.scanInterval = setInterval(() => {
+        this.zone.run(() => {
+          this.devices = [...tempDevices];
+        });
+      }, 500);
+    }
+    this.isScanning = !this.isScanning;
+  }
+}
