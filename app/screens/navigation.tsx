e053e124f50d568b6777d2a9040d950406f9b7f5
RicottaM <jaskulskim.gdynia@gmail.com>
2024-10-22 03:25:48 +0200

    Initialized navigation screen, configured svg display

--- app/screens/navigation.tsx
+++ app/screens/navigation.tsx
@@ -1,15 +1,18 @@
 import React, { useLayoutEffect } from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import { AntDesign, MaterialIcons } from '@expo/vector-icons';
+import { AntDesign, FontAwesome, FontAwesome5, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { useRouter } from 'expo-router';
 import { useHandleRouteChange } from '../hooks/useHandleRouteChange';
+import TestSvg from '../../assets/svg/test.svg';
+import { Screens } from '../enum/screens';
 
 export default function Navigation() {
   const router = useRouter();
   const navigation = useNavigation();
+  const navIconSize = 32;
 
-  const handleRoutePress = useHandleRouteChange();
+  const handleRouteChange = useHandleRouteChange();
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -19,12 +22,24 @@ export default function Navigation() {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.backButtonContainer} onPress={() => router.back()}>
-        <MaterialIcons name="arrow-back-ios" size={32} color="#013b3d" />
-      </TouchableOpacity>
-      {
-        // w tym miejscu umieszczany mape wraz ze stores
-      }
+      <View style={styles.mapContainer}>
+        <TestSvg width={200} height={200} />
+      </View>
+
+      <View style={styles.navbar}>
+        <TouchableOpacity style={styles.navButton} onPress={() => handleRouteChange(Screens.Categories)}>
+          <FontAwesome5 name="th-list" size={navIconSize} color="#013b3d" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.navButton} onPress={() => handleRouteChange(Screens.Code)}>
+          <MaterialCommunityIcons name="qrcode-scan" size={navIconSize} color="#013b3d" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.navButton} onPress={() => handleRouteChange(Screens.Cart)}>
+          <FontAwesome5 name="shopping-basket" size={navIconSize} color="#013b3d" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.navButton} onPress={() => handleRouteChange(Screens.User)}>
+          <FontAwesome name="user" size={navIconSize} color="#013b3d" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -32,7 +47,6 @@ export default function Navigation() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     backgroundColor: '#a0cbb3',
   },
   buttonText: {
@@ -49,5 +63,25 @@ const styles = StyleSheet.create({
     marginTop: 100,
     justifyContent: 'center',
     marginLeft: 40,
+  },
+  navbar: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginHorizontal: 30,
+    paddingBottom: 50,
+    paddingTop: 30,
+  },
+  navButton: {
+    alignItems: 'center',
+    backgroundColor: '#e8fefd',
+    padding: 15,
+    borderRadius: 15,
+    width: 66,
+    height: 62,
+  },
+  mapContainer: {
+    height: 790,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
